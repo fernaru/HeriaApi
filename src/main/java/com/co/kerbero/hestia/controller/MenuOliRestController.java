741f2a1df40cd384d81e56fa119195cde2f6f85c
fernaru <falconp._007@hotmail.com>
2019-04-10 21:50:08 -0500

    modificacion de los rest controllers principales.

--- src/main/java/com/co/kerbero/hestia/controller/MenuOliRestController.java
+++ src/main/java/com/co/kerbero/hestia/controller/MenuOliRestController.java
@@ -1,12 +1,56 @@
 package com.co.kerbero.hestia.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.co.kerbero.hestia.entity.MenuOli;
+import com.co.kerbero.hestia.services.IMenuOilService;
+
 @CrossOrigin(origins= {"http://localhost:8088"})
 @RestController
-@RequestMapping("/apimenu")
+@RequestMapping("/api")
 public class MenuOliRestController {
-
+	
+	@Autowired
+	private IMenuOilService menuService;
+	
+	@GetMapping("/menu")
+	public List<MenuOli> index(){
+		return menuService.findAll();
+	}	
+	
+	@PostMapping("/menu")
+	public ResponseEntity<?> save(@Valid @RequestBody MenuOli menu, BindingResult result){
+		
+		MenuOli menuNew = new MenuOli();
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		if(result.hasErrors()){
+			
+		}
+		try {
+			menuNew = menuService.save(menu);
+			response.put("mensaje","El menu fue creado con exito¡");
+			response.put("menu", menuNew);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		}catch (Exception e) {
+			response.put("mensaje","El menu no pudo ser creado con exito¡");
+			response.put("error", e.getMessage());
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		}
+	}
 }
--- src/main/java/com/co/kerbero/hestia/controller/RolRestController.java
+++ src/main/java/com/co/kerbero/hestia/controller/RolRestController.java
@@ -1,12 +1,27 @@
 package com.co.kerbero.hestia.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.co.kerbero.hestia.entity.Rol;
+import com.co.kerbero.hestia.services.IRolService;
+
 @CrossOrigin(origins= {"http://localhost:8088"})
 @RestController
-@RequestMapping("/apirol")
+@RequestMapping("/api")
 public class RolRestController {
+	
+	@Autowired
+	private IRolService rolService;
+	
+	@GetMapping("/roles")
+	public List<Rol> index(){
+		return rolService.findAll();
+	}
 
 }
